243376b04f7c0b36aadab42a330e82dbad3b1765
Rogério Munhoz <roz@rjmunhoz.me>
2020-09-22 16:47:23 -0300

    Updates

---
+++ precache-manifest.61d17668e04b47109f815bb202e9a2e8.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ab22a6491d58cbbe163fca6980899d9b",
+    "url": "/portfolio-sample-app/index.html"
+  },
+  {
+    "revision": "28279534132cd58ab468",
+    "url": "/portfolio-sample-app/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "d4b2b357656cc49b905f",
+    "url": "/portfolio-sample-app/static/js/2.cfc4381c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/portfolio-sample-app/static/js/2.cfc4381c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "28279534132cd58ab468",
+    "url": "/portfolio-sample-app/static/js/main.1ba02f7f.chunk.js"
+  },
+  {
+    "revision": "5dae28a4d3886bad5fe2",
+    "url": "/portfolio-sample-app/static/js/runtime-main.b85b9d48.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/portfolio-sample-app/static/media/logo.5d5d9eef.svg"
+  }
+]);
